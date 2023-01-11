a4aaa2c6fde2ccaff2830bb3d02315cd896695cd
jarangit <jaran.dch@gmail.com>
2023-01-11 12:45:07 +0700

    feat: link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import Image from 'next/image'
 import Table from './internal-patient-list/components/table'
 import InternalTable from '../components/tables/internalListTable'
+import GoBackButton from '../components/buttons/goBackButton'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
   return (
-      <>
-        <div className='text-3xl font-bold mb-10 animate-fade-in-down'>
-          <div className='text-purple'>รายชื่อผู้ป่วยใน</div>
-          <div>แผนกศัลยกรรมหญิง</div>
-        </div>
-        <div className='animate-fade-in-down'>
-          <InternalTable setShowFormPatient={() => ""} />
-        </div>
-      </>
+    <>
+      <div className='text-3xl font-bold mb-10 animate-fade-in-down'>
+        <GoBackButton onBack={() => router.push('/internal-patient-list')} onShow={true} />
+        <div className='text-purple'>รายชื่อผู้ป่วยใน</div>
+        <div>แผนกศัลยกรรมหญิง</div>
+      </div>
+      <div className='animate-fade-in-down'>
+        <InternalTable setShowFormPatient={() => ""} />
+      </div>
+    </>
   )
 }
